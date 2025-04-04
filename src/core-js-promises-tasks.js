8d83c74486e9dfb6b154a04e3531fcc7b5daabc8
n-andr <150958639+n-andr@users.noreply.github.com>
2025-04-04 20:44:29 +0200

    feat : add getAllResult() and queuePromises()

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -112,8 +112,8 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.all(promises.map((promise) => promise.catch(() => null)));
 }
 
 /**
@@ -134,8 +134,18 @@ function getAllResult(/* promises */) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  const resolutionArray = [];
+  let promissesChain = Promise.resolve();
+  resolutionArray.length = promises.length;
+  promises.forEach((promise, index) => {
+    promissesChain = promissesChain.then(() => {
+      return promise.then((value) => {
+        resolutionArray[index] = value;
+      });
+    });
+  });
+  return promissesChain.then(() => resolutionArray.join(''));
 }
 
 module.exports = {
